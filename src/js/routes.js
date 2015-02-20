fcbb4fcb7a65869e7d9c38479c75a86a7be5cc87
Tim Hooker <timhooker@me.com>
2015-02-19 23:02:42 -0500

    set up basic routes and laid groundwork for templates

---
+++ src/js/routes.js
@@ -0,0 +1,34 @@
+$(function () {
+
+  app.router.add('', function() {
+    console.log('yo man');
+  });
+
+  app.router.add('game/easy', function() {
+    console.log('easy game');
+  });
+
+  app.router.add('game/hard', function() {
+    console.log('hard game');
+  });
+
+  app.router.add('game/win', function() {
+    console.log('you win!!');
+  });
+
+
+
+
+  function processHash() {
+    var hash = location.hash || '#';
+
+    if (!app.router.run(hash.slice(1))) {
+      show404Page();
+    }
+  }
+
+  window.addEventListener('hashchange', processHash);
+
+  processHash();
+
+});
